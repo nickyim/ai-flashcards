eb6bb13b17befd4768baea88a0daa9449cdbd8ca
Aseef Durrani <aseef.durrani11@gmail.com>
2024-08-21 01:08:17 -0400

    npm run build rugging me. setting up stripe identification

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -5,7 +5,7 @@ const systemPrompt = `
 You are a flashcard creator. Your task is to generate concise and effective flashcards based on the given topic or content. Follow these guidelines:
 
 1. Create clear and concise questions for the front of the flashcard.
-2. Provide accurate and informative answers for the back of the flashcard.
+2. Provide accurate and informative answers for the back of the flashcard. Keep the answers brief and to the point.
 3. Ensure that each flashcard focuses on a single concept or piece of information.
 4. Use simple language to make the flashcards accessible to a wide range of learners.
 5. Include a variety of question types, such as definitions, examples, comparisons, and applications.
@@ -30,21 +30,21 @@ Return in the following JSON format
 `;
 
 export async function POST(req) {
-    const openai = new OpenAI()
-    const data = await req.text()
+  const openai = new OpenAI();
+  const data = await req.text();
 
-    const completion = await openai.chat.completions.create({
-        messages: [
-            {role: 'system', content: systemPrompt},
-            {role: 'user', content: data}
-        ],
-        model: "gpt-4o-mini",
-        response_format:{type: 'json_object'}
-    })
+  const completion = await openai.chat.completions.create({
+    messages: [
+      { role: "system", content: systemPrompt },
+      { role: "user", content: data },
+    ],
+    model: "gpt-4o-mini",
+    response_format: { type: "json_object" },
+  });
 
-    console.log(completion.choices[0].message.content)
+  console.log(completion.choices[0].message.content);
 
-    const flashcards = JSON.parse(completion.choices[0].message.content)
+  const flashcards = JSON.parse(completion.choices[0].message.content);
 
-    return NextResponse.json(flashcards.flashcards)
+  return NextResponse.json(flashcards.flashcards);
 }
--- firebase.js
+++ firebase.js
@@ -1,23 +1,25 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore';
+// import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAO-qwlIU6_bTauZCuLgIUBPG1SwXrsqH0",
-  authDomain: "flashcard-saas-23556.firebaseapp.com",
-  projectId: "flashcard-saas-23556",
-  storageBucket: "flashcard-saas-23556.appspot.com",
-  messagingSenderId: "801482584227",
-  appId: "1:801482584227:web:b58983c2ba5dea4dbb4f16",
-  measurementId: "G-L2FH83BJBD"
+  apiKey: "AIzaSyBhzRKkvbMOyzxr_84xZgAdVNKnQQ3HAvU",
+  authDomain: "flashcardsaas-2f9f1.firebaseapp.com",
+  projectId: "flashcardsaas-2f9f1",
+  storageBucket: "flashcardsaas-2f9f1.appspot.com",
+  messagingSenderId: "732912770640",
+  appId: "1:732912770640:web:910b8d931b6ce4fa0e7b55",
+  measurementId: "G-5X1YWSFZGM",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app)
+// const analytics = getAnalytics(app);
+const db = getFirestore(app);
 
-export {db}
+export { db };
