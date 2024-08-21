b26b7072bdfd0b3996bccc1a903119c0e3a7ed6e
Aseef Durrani <aseef.durrani11@gmail.com>
2024-08-21 01:13:17 -0400

    env reorganization

--- firebase.js
+++ firebase.js
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBhzRKkvbMOyzxr_84xZgAdVNKnQQ3HAvU",
-  authDomain: "flashcardsaas-2f9f1.firebaseapp.com",
-  projectId: "flashcardsaas-2f9f1",
-  storageBucket: "flashcardsaas-2f9f1.appspot.com",
-  messagingSenderId: "732912770640",
-  appId: "1:732912770640:web:910b8d931b6ce4fa0e7b55",
-  measurementId: "G-5X1YWSFZGM",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
